a737fa75929d03f72aaa1091207374f280c17db9
kenta suzuki <suzukikenta0210@gmail.com>
2025-04-10 21:06:25 +0900

    firebaseの情報を記載

--- NasKenNo/src/main.tsx
+++ NasKenNo/src/main.tsx
@@ -1,7 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { initializeApp } from "firebase/app";
 import App from './App.tsx'
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+};
+
+const app = initializeApp(firebaseConfig);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
